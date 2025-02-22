4532bf657e8591d5f0f94f3e1e72f3c3f66c046b
Toluwaloju0 <teecoded1999@gmail.com>
2025-02-22 10:55:21 +0100

    telex intagration

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://toluairbnb.tech"],
+    allow_origins=[
+        "http://toluairbnb.tech",
+         "http://staging.telextest.im",
+        "http://telextest.im",
+        "https://staging.telex.im",
+        "https://telex.im",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -23,47 +29,45 @@ app.add_middleware(
 def nginx_status():
     """ The function for the integration endpoint"""
 
-    return JSONResponse(
-        status_code=200,
-        content={
-            "data": {
-                "date": {
-                    "created_at": "2025-02-20",
-                    "updated_at": "2025-02-20"
-                },
-                "descriptions": {
-                    "app_name": "ViewOnce",
-                    "app_description": "A telex app to create view once messages",
-                    "app_logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRRVxPRCAc6HBRl_tR-aMkrCUHZq45ChY_RiwkzwqdF0T8IO52m3Yb9yvp1jjlpyyzVS0&usqp=CAU",
-                    "app_url": "https://viewonce-1.onrender.com/viewOnce",
-                    "background_color": "#fff"
-                },
-                "is_active": True,
-                "integration_type": "modifier",
-                "integration_category": "Monitoring & Logging",
-                "key_features": [
-                    "Find and close view once messages"
-                ],
-                "author": "Toluwaloju Kayode",
-                "settings": [
-                    {
-                        "label": "Find view once",
-                        "type": "checkbox",
-                        "required": True,
-                        "default": True
-                    }
-                ],
-                "target_url": "https://viewonce-1.onrender.com/tagret_url",
-                "endpoints": [
-                    {
-                        "path": "/target_url",
-                        "method": "POST",
-                        "description": "Default endpoint"
-                    }
-                ]
-            }
+    return {
+        "data": {
+            "date": {
+                "created_at": "2025-02-20",
+                "updated_at": "2025-02-20"
+            },
+            "descriptions": {
+                "app_name": "ViewOnce",
+                "app_description": "A telex app to create view once messages",
+                "app_logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRRVxPRCAc6HBRl_tR-aMkrCUHZq45ChY_RiwkzwqdF0T8IO52m3Yb9yvp1jjlpyyzVS0&usqp=CAU",
+                "app_url": "https://viewonce-1.onrender.com/viewOnce",
+                "background_color": "#fff"
+            },
+            "is_active": True,
+            "integration_type": "modifier",
+            "integration_category": "Monitoring & Logging",
+            "key_features": [
+                "Find and close view once messages"
+            ],
+            "author": "Toluwaloju Kayode",
+            "settings": [
+                {
+                    "label": "Find view once",
+                    "type": "checkbox",
+                    "required": True,
+                    "default": True
+                }
+            ],
+            "target_url": "https://viewonce-1.onrender.com/tagret_url",
+            "endpoints": [
+                {
+                    "path": "/target_url",
+                    "method": "POST",
+                    "description": "Default endpoint"
+                }
+            ]
         }
-    )
+    }
+        
 
 
 @app.post("/target_url")
